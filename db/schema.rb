83ac0a21bf69e38a0a0d69f4f1b7635bbad1bb4f
Tom Corley <thomcorley@gmail.com>
2025-06-11 16:04:42 +0100

    Add solid cache migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_07_15_201123) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_11_145606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -224,6 +224,17 @@ ActiveRecord::Schema[8.0].define(version: 2024_07_15_201123) do
     t.boolean "has_image"
     t.json "nutrition"
     t.boolean "show_nutrition"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "tags", force: :cascade do |t|
