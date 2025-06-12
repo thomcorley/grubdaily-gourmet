696b0e4e02a978235e551bafad76edc14770e8c9
Tom Corley <thomcorley@gmail.com>
2025-06-12 14:14:18 +0100

    Update seeds file to pull recipes from prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,106 @@
 # frozen_string_literal: true
-class RecipeSeeder
-  def self.seed
-    count = 0
-    Dir.glob("lib/yaml_recipes/*.yaml") do |yaml_filename|
-      filepath = Rails.root.join(yaml_filename)
-      yaml_file = File.read(filepath)
+require 'open-uri'
+require 'json'
 
-      ActiveRecord::Base.transaction do
-        importer = RecipeImporter.new(yaml_file)
-        recipe_id = importer.save_recipe
-        importer.save_ingredients(recipe_id)
-        importer.save_method_steps(recipe_id)
+puts "Destroying existing recipes, entries, and tags..."
+Recipe.destroy_all
+Entry.destroy_all
+Tag.destroy_all
+puts "Existing data cleared."
 
-        recipe = Recipe.find(recipe_id)
-        recipe_image = "#{recipe.image_name}.jpg"
+url = 'https://api.grubdaily.com/recipes'
+puts "Fetching recipes from #{url}..."
 
+begin
+  json_data = URI.open(url).read
+  recipes_data = JSON.parse(json_data)
+rescue OpenURI::HTTPError => e
+  puts "Error fetching recipes: #{e.message}. The API might be down."
+  exit
+rescue JSON::ParserError => e
+  puts "Error parsing JSON response: #{e.message}."
+  exit
+end
+
+puts "Found #{recipes_data.count} recipes. Starting import..."
+
+recipes_data.each do |recipe_data|
+  next unless recipe_data['published']
+
+  Recipe.transaction do
+    puts "Processing recipe: #{recipe_data['title']}"
+
+    entry_content = recipe_data['introduction']
+    entry_excerpt = recipe_data['summary']
+    entry_slug = recipe_data['slug']
+
+    tags = []
+    if recipe_data['category'].present?
+      tags << Tag.find_or_create_by!(name: recipe_data['category'].downcase)
+    end
+    if recipe_data['recipe_type'].present?
+      tags << Tag.find_or_create_by!(name: recipe_data['recipe_type'].downcase)
+    end
+
+    entry = Entry.create!(
+      content: entry_content,
+      summary: entry_excerpt,
+      slug: entry_slug,
+      tags: tags.uniq
+    )
+
+    recipe = entry.create_entryable!(
+      type: 'Recipe',
+      title: recipe_data['title'],
+      total_time: recipe_data['total_time'],
+      serves: recipe_data['serves'],
+      makes: recipe_data['makes'],
+      makes_unit: recipe_data['makes_unit'],
+      published_at: recipe_data['published_at'],
+      marked_for_promotion_at: recipe_data['marked_for_promotion_at']
+    )
+
+    image_url = recipe_data.dig('images', 'main', 'large')
+    if image_url.present? && image_url.start_with?('http')
+      begin
+        puts "  Attaching image from #{image_url}..."
+        image_file = URI.open(image_url)
         recipe.image.attach(
-          io: File.open("#{Rails.root.to_s}/app/assets/images/#{recipe.image_name}.jpg"),
-          filename: "#{recipe.image_name}.jpg"
+          io: image_file,
+          filename: File.basename(URI.parse(image_url).path)
         )
-
-        recipe.save!
-
-        Recipe.update_all(published_at: Date.today)
-
-        count += 1
-        puts "Recipe created: #{yaml_filename}"
-      rescue Psych::SyntaxError => e
-        puts "Error parsing YAML file #{yaml_filename}. Message: #{e}"
       rescue StandardError => e
-        puts "Recipe (#{yaml_filename}) could not be created: #{e}"
+        puts "  Could not attach image for #{recipe.title}. Error: #{e.message}"
+      end
+    else
+      puts "  No valid image found for #{recipe.title}."
+    end
+
+    recipe_data['ingredient_sets'].sort_by { |is| is['position'] }.each do |set_data|
+      ingredient_set = recipe.ingredient_sets.create!(
+        title: set_data['title'],
+        position: set_data['position']
+      )
+
+      set_data['ingredient_entries'].each do |entry_data|
+        ingredient_set.ingredient_entries.create!(
+          quantity: entry_data['quantity'],
+          unit: entry_data['unit'],
+          size: entry_data['size'],
+          modifier: entry_data['modifier'],
+          original_string: entry_data['original_string'],
+          ingredient_name: entry_data['ingredient_name']
+        )
       end
     end
 
-    puts "Imported #{count} recipes"
-
-    puts "Attaching images to recipes..."
-
-    Recipe.all.each do |recipe|
-      recipe.image.attach(
-        io: File.open("#{Rails.root.to_s}/app/assets/images/#{recipe.image_name}.jpg"),
-        filename: "#{recipe.image_name}.jpg"
+    recipe_data['method_steps'].sort_by { |ms| ms['position'] }.each do |step_data|
+      recipe.method_steps.create!(
+        position: step_data['position'],
+        description: step_data['description']
       )
-      puts "Attached image for #{recipe.title}"
-    rescue StandardError
-      next
     end
   end
 end
 
-RecipeSeeder.seed if Rails.env == "development"
+puts "Seed import complete."
